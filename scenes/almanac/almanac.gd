5e1cde22fd4a81c0fdd53a010bf9b977ad7684ad
Aks-l <wiersdalenaksel@gmail.com>
2025-09-19 13:07:48 +0200

    Removed duplicate almanac files

--- scenes/almanac/almanac.gd
+++ scenes/almanac/almanac.gd
@@ -5,6 +5,7 @@ var card_scene := preload("res://scenes/almanac/enemyCard.tscn")
 func _ready() -> void:
 	var enemies = EnemyDatabase.enemies.keys()
 	for enemy in enemies:
+		
 		var data: EnemyData = EnemyDatabase.enemies[enemy]
 		var card = card_scene.instantiate()
 		grid.add_child(card)
