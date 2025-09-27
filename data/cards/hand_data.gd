ae85653c72368022eeab9bfc3cd0b3fb4c2cf4a0
katharki <kathakiv@outlook.com>
2025-09-27 23:03:25 +0200

    updated existing cards with new array(beats)

--- data/cards/hand_data.gd
+++ data/cards/hand_data.gd
@@ -7,6 +7,9 @@ class_name HandData
 @export var name: String # desplays name, example "Rock"
 @export var beats: Array[String] #what beats what
 
+#undiscovered by default
+@export var discovered: bool = false
+
 #how any cpies of each card
 #temporarly because max count will change
 @export var max_count: int = 3
