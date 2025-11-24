7d45687af32192a5a4eae8428aa5c5828943d453
Aks-l <wiersdalenaksel@gmail.com>
2025-11-24 14:49:31 +0100

    Generated biome data

--- data/biomes/biome_data.gd
+++ data/biomes/biome_data.gd
@@ -1,10 +1,14 @@
 extends Resource
 class_name BiomeData
 
+@export_group("Metadata")
 @export var id: int
 @export var name: String
 @export var difficulty: int
+@export var discovered: bool = false
+@export var encountered: bool = false # prevent encountering same biome twice in one run
 
+@export_group("Color palette")
 @export var ocean_color: Color 
 @export var shallow_water_color: Color 
 @export var sand_color: Color 
