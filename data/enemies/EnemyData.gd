47b6348ce66b78179b58cb1851dd6df3f2737735
Kath k <katharki@stud.ntnu.no>
2025-12-10 16:23:36 +0100

    added encounterCount to enemyData

--- data/enemies/EnemyData.gd
+++ data/enemies/EnemyData.gd
@@ -8,10 +8,13 @@ signal update_hand_visuals(hand: HandData)
 @export var sprite: Texture2D
 @export var name: String
 @export var description: String
+
 @export var discovered: bool = false
+@export var encounter_count: int = 0
+
 @export var biome: String = "neutral"
 #for difficulty
-@export var tier: int = 1 #1 er tidlig, 2 is mid, and 3 is last biome 
+@export var level: int = 1 #1 er tidlig, 2 is mid, and 3 is last biome 
 
 #Dictionary of HandData, number of copies
 @export var deck: Dictionary = {}
